9936a7613043aa80df9af48af01b7515f7869916
Simon-Bertrand <simonbertrand.contact@gmail.com>
2021-08-17 09:56:01 +0200

    Continuous dev : Improving the CLI

--- ClustersFeatures/src/_confusion_hypersphere.py
+++ ClustersFeatures/src/_confusion_hypersphere.py
@@ -6,8 +6,6 @@ class __ConfusionHypersphere:
     def confusion_hypersphere_matrix(self, **args):
         """Returns the confusion hypersphere matrix.
 
-        This function counts for each cluster the number of elements contained in each hypersphere centred on centroids.
-
         :param float radius_choice: The radius of the hypersphere.
         :param str counting_type: a str in ['including', 'excluding'].
             If including, then the elements belonging cluster i and contained inside the hypersphere of centroid i are counted (for i=j). If excluding, then they're not counted.
